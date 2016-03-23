db9ae252bde2eb22a34062f45271fc12dd64f7c2
Dustin Hershman <dustinh17@gmail.com>
2016-03-23 11:22:26 -0400

    Removed unused component code

--- cleanDateWithMoment.js
+++ cleanDateWithMoment.js
@@ -4,55 +4,39 @@
  * @version 1.0.0
  */
 
-(function(root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		define(['moment', 'jquery'], factory);
-	} else if (typeof module === 'object' && module.exports) {
-		module.exports = factory(['moment', 'jquery']);
-	} else {
-		if (window.comp == null) root.comp = {};
-		if (window.comp.utils == null) root.comp.utils = {};
-
-		root.comp.utils.cleanDateWithMoment = factory(root.moment, root.$);
-	}
-}(this, function(moment, $) {
-
-	var cleanDateWithMoment = {
-		/**
-		 * cleans a single date string sent to it
-		 * @method   cleanOneAttribute
-		 * @memberOf cleanDate
-		 * @param    {string}          date   the date string to be formatted
-		 * @param    {string}          format the format the date should be in
-		 * @return   {string}                 returns the fixed date
-		 */
-		cleanOneAttribute: function(date, format) {
-			format = format || 'MMM DD, YYYY';
-			return moment(date).format(format);
-		},
-		/**
-		 * Recursively crawls through an array of objects that could have more arrays of objects containing dates and fixes them.
-		 * This is a Synchronous function
-		 * @method   deepClean
-		 * @memberOf cleanDate
-		 * @param    {array|object}         items  array of objects to be looped through
-		 * @param    {array}         attr    an array of keys to look for in order to fix their date
-		 * @param    {string}         format the format the date should be in
-		 * @return   {array}                returns the same array back with the clean dates placed
-		 */
-		deepClean: function(items, attr, format) {
-			format = format || 'MMM DD, YYYY';
-			var key;
-			for (key in items) {
-				if (items.hasOwnProperty(key) && $.inArray(key, attr) > -1) {
-					items[key] = moment(items[key]).format(format);
-				} else if (typeof items[key] === 'object') {
-					items[key] = this.deepClean(items[key], attr, format);
-				}
-			}
-			return items;
-		}
-	}
-
-	return cleanDateWithMoment;
-}));
+var cleanDateWithMoment = {
+    /**
+     * cleans a single date string sent to it
+     * @method   cleanOneAttribute
+     * @memberOf cleanDate
+     * @param    {string}          date   the date string to be formatted
+     * @param    {string}          format the format the date should be in
+     * @return   {string}                 returns the fixed date
+     */
+    cleanOneAttribute: function(date, format) {
+        format = format || 'MMM DD, YYYY';
+        return moment(date).format(format);
+    },
+    /**
+     * Recursively crawls through an array of objects that could have more arrays of objects containing dates and fixes them.
+     * This is a Synchronous function
+     * @method   deepClean
+     * @memberOf cleanDate
+     * @param    {array|object}         items  array of objects to be looped through
+     * @param    {array}         attr    an array of keys to look for in order to fix their date
+     * @param    {string}         format the format the date should be in
+     * @return   {array}                returns the same array back with the clean dates placed
+     */
+    deepClean: function(items, attr, format) {
+        format = format || 'MMM DD, YYYY';
+        var key;
+        for (key in items) {
+            if (items.hasOwnProperty(key) && $.inArray(key, attr) > -1) {
+                items[key] = moment(items[key]).format(format);
+            } else if (typeof items[key] === 'object') {
+                items[key] = this.deepClean(items[key], attr, format);
+            }
+        }
+        return items;
+    }
+}
